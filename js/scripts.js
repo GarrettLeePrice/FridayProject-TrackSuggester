8efbe0888be67069906562ad08e60aa9e74553ef
Garrett Price <garrettleeprice@gmail.com>
2016-08-12 10:18:07 -0700

    add show results button, and connects it to results output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
+  $("form#selections").submit(function(event) {
     var companyInput = $("input#companyType").val();
     var projectInput = $("input#projectType").val();
     var experienceInput = $("input#experience").val();
     var locationInput = $("input#location").val();
     var seasonInput = $("input#season").val();
 
-    $(".companyType").text(person1Input);
-    $(".projectType").text(person2Input);
-    $(".experience").text(animalInput);
-    $(".location").text(exclamationInput);
-    $(".season").text(verbInput);
+    $(".companyType").text(companyInput);
+    $(".projectType").text(projectInput);
+    $(".experience").text(experienceInput);
+    $(".location").text(locationInput);
+    $(".season").text(seasonInput);
 
 
-    $("#results").show();
+    $("#yourResults").show();
 
     event.preventDefault();
 
